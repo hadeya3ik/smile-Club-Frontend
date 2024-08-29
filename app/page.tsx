c5852e540e5b3b6f0f8aa30a928b5f5781eafb99
hadeya3ik <hadeyaik@gmail.com>
2024-08-28 22:13:14 -0400

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+'use client'
+import { ReactLenis } from 'lenis/react';
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <div>this is hadeya . me </div>
-    </main>
+  return (<ReactLenis root>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div>this is hadeya . me </div>
+      </main>
+    </ReactLenis>
   );
 }
