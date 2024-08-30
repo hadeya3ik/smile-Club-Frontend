4db700e166bc173b4d2db7f704630b8d01418bea
hadeya3ik <133555427+hadeya3ik@users.noreply.github.com>
2024-08-30 12:32:22 -0400

    merge update

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${ppNeueMontreal.className} bg-green-100`}>{children}</body>
+      <body className={`${ppNeueMontreal.className}`}>{children}</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 'use client'
+
 import { ReactLenis } from 'lenis/react';
 import Zoom from "@/components/Zoom";
 import ParallaxBg from "@/components/ParallaxBg";
 export default function Home() {
+
   return (
     <ReactLenis root>
     <main className="">
