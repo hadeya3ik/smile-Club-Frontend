05097a08d51f4cf8f1986cf5b078f8963fb0ad50
hadeya3ik <133555427+hadeya3ik@users.noreply.github.com>
2024-09-06 12:13:10 -0400

    about us finalized

--- components/About.tsx
+++ components/About.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import ParallaxBg from './ParallaxBg'
+import pic1 from '@/images/pexels11.jpg'
+import pic2 from '@/images/pexels12.jpg'
+import Image from 'next/image'
+
 function About() {
   return ( <main className='font-ppNeueMontreal'>
-    <div className='md:p-24 sm:p-16 p-8 flex flex-col'>
-        <h1 className='md:text-5xl sm:text-4xl text-3xl'>
-            About Us 
-        </h1>
+    <div className='md:p-24 sm:p-16 p-8 flex flex-col gap-12'>
+        <div className='flex justify-between'>
+            <h1 className='md:text-5xl sm:text-4xl text-3xl self-center'>
+                About Us 
+            </h1>
+            <div className='flex gap-4'>
+               <Image src={pic2}  height={300}  alt="" objectFit="cover" layout='fixed' className='self-end' />
+               <Image src={pic1}  height={300}  alt="" objectFit="cover" layout='fixed' className='self-end pb-8' />
+            </div>
+        </div>
         <div className='flex pt-4'>
             <p className='max-w-lg'>
                 Modern Walls is an interior design studio specializing in renovations, dedicated to transforming spaces. Our expertise lies in creating modern, stylish interiors that seamlessly blend innovation with timeless design elements, ensuring every work reflects our commitment to craftsmanship.
@@ -14,6 +24,8 @@ function About() {
                 We focus on designing spaces that meet the needs of the people who use them.
             </p>
         </div>
+        
+    
     </div>
     <ParallaxBg/>
     </main>
--- components/ParallaxBg.tsx
+++ components/ParallaxBg.tsx
@@ -1,7 +1,7 @@
 'use client'
 import React, { useRef } from 'react';
 import Image from 'next/image';
-import Background from '@/images/pexels3.jpg';
+import Background from '@/images/pexels13.jpg';
 import { useScroll, useTransform, motion } from 'framer-motion';
 
 export default function ParallaxBg() {
@@ -17,10 +17,10 @@ export default function ParallaxBg() {
     return (
         <div
             ref={container} 
-            className='relative flex items-center justify-center h-[300px] overflow-hidden'
+            className='relative flex items-center justify-center h-[900px] overflow-hidden'
             style={{ clipPath: "polygon(0% 0, 100% 0%, 100% 100%, 0 100%)" }}
         >
-            <div className='fixed top-[-5vh] left-0 h-[120vh] w-full'>
+            <div className='fixed top-[-5vh] left-0 h-[100vh] w-full'>
                 <motion.div style={{ y }} className='relative w-full h-full'>
                     <Image src={Background} fill alt="image" style={{ objectFit: "cover" }} />
                 </motion.div>
