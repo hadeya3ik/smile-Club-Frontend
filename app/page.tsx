9fde7fb48a2cb4c7905d9011eea95211929b87e6
hadeya3ik <133555427+hadeya3ik@users.noreply.github.com>
2024-09-07 23:27:28 -0400

    added folder about page

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,11 @@ import { ReactLenis } from 'lenis/react';
 import NavBar from '@/components/NavBar';
 import Footer from '@/components/Footer';
 import Cards from '@/components/Cards';
+
 export default function Home() {
   return ( <ReactLenis root>
     <main className="">
-      {/* <NavBar/> */}
       <Cards/>
-      <div className='h-screen'></div>
     </main>
     </ReactLenis>
   );
--- components/Cards.tsx
+++ components/Cards.tsx
@@ -1,23 +1,25 @@
-import { Content } from 'next/font/google'
-import React from 'react'
-import Mission from '@/images/mission.svg'
-import Story from '@/images/story.svg'
-import Vision from '@/images/vision.svg'
+import React from 'react';
+import Mission from '@/images/mission.svg';
+import Story from '@/images/story.svg';
+import Vision from '@/images/vision.svg';
 
 function Cards() {
     return (
-        <div className='relative pt-40 border'>
-            <div className='h-screen w-full flex items-center justify-center sticky top-40' >
-                <Mission className=""/>
+        <div className='relative mt-40 border max-w-[80vw]'>
+            <div className='sticky' style={{ top: '0px' }}>
+                <Mission className="" />
             </div>
-            <div className='h-screen w-full flex items-center justify-center sticky top-0' style={{top: `calc(-10% + ${1 * 230}px)`}}>
-                <Story className=""/>
+            <div className='sticky' style={{ top: '10vh' }}> 
+                <Vision className="" />
             </div>
-            <div className='h-screen w-full flex items-center justify-center sticky top-0' style={{top: `calc(-10% + ${2 * 500}px)`}}>
-                <Vision className=""/>
+            <div className='sticky' style={{ top: '20vh' }}> 
+                <Story className="" />
+            </div>
+            <div className='sticky' style={{ top: '30vh' }}>
+                <div className='h-[30vh] w-[100px]'></div>
             </div>
         </div>
-      )
-    }
+    );
+}
 
-export default Cards
+export default Cards;
