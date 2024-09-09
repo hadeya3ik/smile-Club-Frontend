7ce190ec65a84f298a8fc9922cbf5510efb0cf62
hadeya3ik <133555427+hadeya3ik@users.noreply.github.com>
2024-09-09 16:41:21 -0400

    base svg home page

--- app/page.tsx
+++ app/page.tsx
@@ -6,19 +6,25 @@ import Cards from '@/components/Cards';
 import Hero from '@/images/Hero.svg'
 import Mask from '@/components/Mask';
 import Clouds from '@/images/Clouds.svg'
+import Sunset from '@/images/Sunset.svg'
+import Clouds1 from '@/images/Clouds-1.svg'
+import Clouds2 from '@/images/Clouds-2.svg'
+import CloudsA from '@/images/CLOUDSA.svg'
+
+
 export default function Home() {
   return ( 
   <ReactLenis root>
     <main className="bg-[#599CFF]">
       <NavBar/>
-      <div className=''>
-        <div className='absolute top-[30%]'>
-          <Mask/>
-        </div>
-        <Hero className='relative z-10 top-0' />
+      <Hero className=''/>
+      <Clouds2/>
+      <div className="bg-[#F4F2E3] h-screen">
+      <Clouds1 className="bg-[#F4F2E3]"/>
+      <div className="bg-[#FBD0F4] h-screen"></div>
       </div>
-      
-      
+      {/* <Clouds1 className="bg-[#F4F2E3]"/> */}
+      <Sunset className="bg-[#FBD0F4]"/>
     </main>
   </ReactLenis>
   );
